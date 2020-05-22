d48523cb06c16b374e798d61eccb08f02d85cd72
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2020-05-22 23:25:24 +0200

    basic demo

--- UserDefaultsCaching/AppDelegate.swift
+++ UserDefaultsCaching/AppDelegate.swift
@@ -11,10 +11,11 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UserDefaults.standard.set(Data(repeating: 1, count: 100_000_000), forKey: "data")
+        
         return true
     }
 
